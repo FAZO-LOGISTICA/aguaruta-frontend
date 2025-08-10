f5b9966b781f8435f21f12720aa58854bfbc405d
FAZO-LOGISTICA <gustavo.oliva.miranda@gmail.com>
2025-08-09 21:23:33 -0400

    Delete src/pages/RutasActivas.js

--- src/pages/RutasActivas.js
+++
@@ -1,7 +0,0 @@
-
-// Código completo profesional de la tabla se insertará luego si es necesario
-import React from 'react';
-
-export default function RutasActivas() {
-  return <div>Tabla de rutas activas aquí</div>;
-}
